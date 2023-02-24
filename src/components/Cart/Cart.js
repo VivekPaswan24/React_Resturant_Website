4ee53ab5e8dab1c1043d4682dfa16d503809f375
VivekPaswan24 <vivek.paswan0@gmail.com>
2023-02-24 20:42:26 +0530

    Cart Items Are visible in the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,27 +2,45 @@ import React, { useContext } from "react";
 import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
-   const cartCtx=useContext(CartContext);
-   const filteredMealItemName=Array.from(cartCtx.items.reduce((map,item)=>map.set(item.id,item),new Map()).values());
-   const mealItemName=filteredMealItemName.map((item)=>{
-    return <li key={item.id}>{item.name}</li>
-   })
-   console.log(mealItemName)
+  const cartCtx = useContext(CartContext);
+
+  const addItemHanlder = (item) => {
+    cartCtx.addItem({ ...item, quantity: 1 });
+  };
+
+  const removeItemHandler = (item) => {
+    cartCtx.removeItem(item);
+  };
+
+  const mealItemName = cartCtx.items.map((item) => {
+    return (
+      <CartItem
+        key={item.id}
+        name={item.name}
+        price={item.price}
+        quantity={item.quantity}
+        onAdd={addItemHanlder.bind(null, item)}
+        onRemove={removeItemHandler.bind(null, item)}
+      />
+    );
+  });
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   return (
     <Modal onClick={props.onClose}>
       <div>
-        <ul className={classes["cart-items"]}>
-          {mealItemName}
-        </ul>
+        <ul className={classes["cart-items"]}>{mealItemName}</ul>
       </div>
       <div className={classes.total}>
         Total Amount
-        <div>35.62</div>
+        <div>{totalAmount}</div>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>Cancle</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Cancle
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,15 +2,59 @@ import React, { useState } from "react";
 import CartContext from "./cart-context";
 
 const CartProvider = (props) => {
-  const [items,setItems]=useState([])
-  const [totalAmount,setTotalAmount]=useState(0)
+  const [items, setItems] = useState([]);
+  const [totalAmount, setTotalAmount] = useState(0);
   const addItemToCartHandler = (item) => {
-    setItems((prevItems)=>{
-      return [...prevItems,item]
-    })
+    setTotalAmount((prevAmount) => {
+      prevAmount = prevAmount + item.quantity * item.price;
+      return prevAmount;
+    });
+    setItems((prevItems) => {
+      let updatedItems;
+      const existingItemIndex = prevItems.findIndex(
+        (ele) => ele.id === item.id
+      );
+      const existingItem = prevItems[existingItemIndex];
+      if (existingItem) {
+        const updatedItem = {
+          ...existingItem,
+          quantity: Number(existingItem.quantity) + Number(item.quantity),
+        };
+        updatedItems = [...prevItems];
+        updatedItems[existingItemIndex] = updatedItem;
+      } else {
+        updatedItems = prevItems.concat(item);
+      }
+
+      return updatedItems;
+    });
   };
 
-  const removeItemFromCartHandler = () => {};
+  const removeItemFromCartHandler = (item) => {
+    let updatedItems;
+    setTotalAmount((prevAmount) => {
+      prevAmount = prevAmount - item.price;
+      return prevAmount;
+    });
+    setItems((prevItems) => {
+      const quantity = Number(item.quantity);
+      if (quantity === 1) {
+        updatedItems = prevItems.filter((ele) => ele.id !== item.id);
+      } else {
+        const existingItemIndex = prevItems.findIndex(
+          (ele) => ele.id === item.id
+        );
+        const existingItem = prevItems[existingItemIndex];
+        let updatedItem = {
+          ...existingItem,
+          quantity: Number(existingItem.quantity) - 1,
+        };
+        updatedItems = [...prevItems];
+        updatedItems[existingItemIndex] = updatedItem;
+      }
+      return updatedItems;
+    });
+  };
 
   const cartContext = {
     items: items,
